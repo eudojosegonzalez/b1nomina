8018fa330765d4d8a902e2305efcdde6d4a1462b
Eudo Gonzalez <eudojosegonzalez@hotmail.com>
2024-01-24 21:58:06 -0400

    commit unidades de pacto 2024-01-24

---
+++ .history/routers/afp_20240123140412.py
@@ -0,0 +1,105 @@
+from fastapi import APIRouter,Body
+from fastapi import Path,Query, Depends
+from fastapi.responses import JSONResponse
+#from pydantic import BaseModel
+from config.database import engine, Base
+#from schemas.user import Bancos
+
+#from typing import  Optional, List
+from typing import  List
+from config.database import Session
+# dependencia que coinvierte los objketos tipo Bd a json
+from fastapi.encoders import jsonable_encoder
+from utils.jwt_managr import create_token,validate_token
+
+
+# importamos el controlador 
+from controller.afp import AFPController
+
+#from middleware.error_handler import ErrorHandler
+from middleware.jwt_bearer import JWTBearer
+
+
+# esta variable define al router
+afp_router = APIRouter(prefix="/V1.0")
+
+# -------- Rutas AFP ------------
+# ruta para crear las Instituciones AFP
+@afp_router.post ('/create_afp', 
+tags=["Instituciones AFP"],status_code=200, 
+#dependencies=[Depends(JWTBearer())],
+)
+def create_afp():
+    return JSONResponse (status_code=201,content={"message":"Se creo una institución AFP en el sistema"})  
+
+# ruta para listar las Instituciones AFP 
+@afp_router.get ('/afp', 
+tags=["Instituciones AFP"],
+status_code=200, 
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },             
+        500: {
+            "description": "Su session ha expirado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Su session ha expirado",
+                            "estado":"Signature has expired"
+                        }
+                    } 
+                }       
+            },                         
+        520: {
+            "description": "Ocurrió un error que no pudo ser controlado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Ocurrió un error que no pudo ser controlado",
+                            "estado":"System Error"
+                        }
+                    } 
+                }       
+            },                       
+    }
+)
+def list_afp(page : int = 1, records : int = 20)->dict:
+    db = Session()
+    # almacenamos el listado de usarios en un resultset
+    result = AFPController(db).list_AFP(page,records)
+    # debemnos convertir los objetos tipo BD a Json
+    if (result):
+        return JSONResponse(status_code=200,content=jsonable_encoder(result))    
+    else:
+        return JSONResponse(status_code=404,content={"message":"No hay registros que mostrar"})     
+
+# ruta para consultar una Institución AFP por Id
+@afp_router.get ('/afp/{id}', 
+tags=["Instituciones AFP"],
+status_code=200, 
+dependencies=[Depends(JWTBearer())]
+)
+def get_afp(id: int):
+    return JSONResponse (status_code=200,content={"message":"Obtener una institucon AFP por ID"})  
+
+# ruta para actualizar una institución AFP por Id
+@afp_router.put ('/afp/{id}/update', 
+tags=["Instituciones AFP"],
+status_code=200, 
+dependencies=[Depends(JWTBearer())]
+)
+def update_afp(id: int):
+    return JSONResponse (status_code=200,content={"message":"Actualizar una Institución AFP por ID"})      
---
+++ .history/routers/unidades_pacto_20240124182400.py
@@ -0,0 +1,427 @@
+'''
+Rutas de Unidades de Pacto
+2024-01
+'''
+import os
+
+#importamos la libreria para cargar los archivos de entorno
+import dotenv
+
+from fastapi import APIRouter,Body
+from fastapi import Path,Query, Depends
+from fastapi.responses import JSONResponse
+#from pydantic import BaseModel
+#from utils.jwt_managr import create_token,validate_token
+from config.database import engine, Base
+
+#from typing import  Optional, List
+from typing import  List
+from config.database import Session
+# dependencia que coinvierte los objetos tipo Bd a json
+from fastapi.encoders import jsonable_encoder
+from utils.jwt_managr import create_token,validate_token
+
+#importamos el schema de datos del banco
+from schemas.unidades_pacto import UnidadesPacto as UnidadesPactoSchema
+
+
+# importamos el controlador 
+from controller.unidades_pacto import UnidadesPactoController
+
+
+#from middleware.error_handler import ErrorHandler
+from middleware.jwt_bearer import JWTBearer
+
+#cargamos las variables de entorno
+dotenv.load_dotenv()
+
+
+# esta variable define al router
+unidades_pacto_router = APIRouter(prefix="/V1.0")
+
+# -------- Rutas Bancos ------------
+# ruta para crear las Instituciones Bancaria
+@unidades_pacto_router.post ('/create_banco', 
+tags=["UnidadesPacto"],
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        201: {
+            "description": "Se creo un Banco en el sistema",
+            "content": { 
+                "application/json":{
+                    "example":
+                        {
+                            "message":"Se creo un Banco en el sistema",
+                            "newUserId":"1"
+                        }
+                    } 
+                }       
+            },
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },            
+        500: {
+            "description": "Su session ha expirado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Su session ha expirado",
+                            "estado":"Signature has expired"
+                        }
+                    } 
+                }       
+            },              
+        520: {
+            "description": "Ocurrió un error que no pudo ser controlado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Ocurrió un error que no pudo ser controlado",
+                            "estado":"System Error"
+                        }
+                    } 
+                }       
+            },                       
+    }                      
+)
+def create_unidades_pacto(banco:bancosSchema, userCreatorId : int = Query (ge=1, le=os.getenv("MAX_ID_USERS")))->dict:
+    db = Session()
+    result=UnidadesPactoController(db).create_unidades_pacto(banco,userCreatorId)
+    # evaluamos el resultado
+    estado=result['result']
+
+    if (estado=="1") :
+        # se inserto el registro sin problemas
+        newBanco=result['data']
+        return JSONResponse (status_code=201,content={"message":"Se creo un Banco en el sistema","Banco":jsonable_encoder(newBanco)})     
+    elif  (estado=="-1"):
+        # el username ya existe no puede volver a insertarlo
+        return JSONResponse (status_code=521,content={"message":"existe un banco con este nombre, no puede volver a crearlo","Banco":jsonable_encoder(result['data'])})     
+    elif (estado=="-2"):
+        return JSONResponse (status_code=521,content={"message":"existe un banco con este codigo, no puede volver a crearlo","Banco":jsonable_encoder(result['data'])})     
+
+    else:
+        return JSONResponse (status_code=520,content={"message":"Ocurrió un error que no pudo ser controlado","estado":result})    
+             
+
+# ruta para listar los bancos en el sistema
+@unidades_pacto_router.get ('/list_bancos', 
+tags=["UnidadesPacto"],
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },             
+        500: {
+            "description": "Su session ha expirado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Su session ha expirado",
+                            "estado":"Signature has expired"
+                        }
+                    } 
+                }       
+            },                         
+        520: {
+            "description": "Ocurrió un error que no pudo ser controlado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Ocurrió un error que no pudo ser controlado",
+                            "estado":"System Error"
+                        }
+                    } 
+                }       
+            },                       
+    }
+)
+def list_bancos(page : int = 1, records : int = 20)->dict:
+    db = Session()
+    # almacenamos el listado de usarios en un resultset
+    result = UnidadesPactoController(db).list_bancos(page,records)
+
+    # debemnos convertir los objetos tipo BD a Json
+    if (result):
+        return JSONResponse(status_code=200,content=jsonable_encoder(result))    
+    else:
+        return JSONResponse(status_code=404,content={"message":"No hay registros que mostrar"}) 
+
+
+# ruta para consultar una Institución Bancaria por Id
+@unidades_pacto_router.get ('/bancos/{id}', 
+tags=["UnidadesPacto"],
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        200: {
+                "description": "Banco encontrado",
+                "content": { 
+                    "application/json":
+                        { 
+                            "example":
+                                {
+                                    "message":"Banco encontrado",
+                                    "data": "{'id':'1','rut': '12345678912','rut': '12345678912', 'nombres': 'Pedro ', 'apellido_paterno': 'Perez', 'apellido_materno': 'Martinez', 'fecha_nacimiento': '1990-01-01','sexo_id': '1', 'estado_civil_id': '1', 'nacionalidad_id': '1','username': 'pperez' 'password': '12345678','activo':'1','created':'1990-01-01 10:00','updated':'1990-01-01 11:00','creator_user':'1','updater_user':'1' }",
+                                }
+                        } 
+                    
+                } 
+            },         
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },  
+        404: {
+            "description": "Banco no encontrado",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Banco no encontrado"
+                        }
+                    } 
+                }       
+            },   
+        500: {
+            "description": "Su session ha expirado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Su session ha expirado",
+                            "estado":"Signature has expired"
+                        }
+                    } 
+                }       
+            },                                                           
+    }   
+)
+def get_unidades_pacto(id: int):
+    db = Session()
+    # almacenamos el listado de usarios en un resultset
+    result = UnidadesPactoController(db).get_unidades_pacto(id)
+    # debemnos convertir los objetos tipo BD a Json
+    if (result):
+        if (result["result"]=="1"):
+            data=result['data']
+            return JSONResponse(status_code=200,content={"Banco":jsonable_encoder(data)})    
+        else:
+            return JSONResponse(status_code=404,content={"message":"Banco no encontrado"})     
+    
+    
+    return JSONResponse(status_code=404,content={"message":"Banco no encontrado"})      
+
+
+
+# ruta para listar los bancos en el sistema
+@unidades_pacto_router.get ('/bancos/{id}/list_historico_banco', 
+tags=["UnidadesPacto"],
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },             
+        500: {
+            "description": "Su session ha expirado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Su session ha expirado",
+                            "estado":"Signature has expired"
+                        }
+                    } 
+                }       
+            },                         
+        520: {
+            "description": "Ocurrió un error que no pudo ser controlado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Ocurrió un error que no pudo ser controlado",
+                            "estado":"System Error"
+                        }
+                    } 
+                }       
+            },                       
+    }
+)
+def list_history_bancos(page : int = 1, records : int = 20, id : int =Path(ge=1, lt=1000))->dict:
+    db = Session()
+    # almacenamos el listado de usarios en un resultset
+    result = UnidadesPactoController(db).list_history_bancos(page,records,id)
+
+    # debemnos convertir los objetos tipo BD a Json
+    if (result):
+        return JSONResponse(status_code=200,content=jsonable_encoder(result))    
+    else:
+        return JSONResponse(status_code=404,content={"message":"No hay registros que mostrar"}) 
+
+
+# ruta para buscar un banco por nombre o codigo
+@unidades_pacto_router.get ('/search_bancos', 
+tags=["UnidadesPacto"],
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        200: {
+                "description": "Bancos encontrado",
+                "content": { 
+                    "application/json":
+                        { 
+                            "example":
+                                {
+                                    "message":"Bancos encontrado",
+                                    "data": "{'codigo': '001',''nombre':'BANCO CHILE Y EDWARDS','nomina': 1,'created':'2023-01-01','updated':'2023-01-01','creator_user':1,'updater_user':1 }",
+                                }
+                        } 
+                    
+                } 
+            },         
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },  
+        404: {
+            "description": "La búsqueda no arrojó resultados",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"La búsqueda no arrojó resultados"
+                        }
+                    } 
+                }       
+            },                                       
+        520: {
+            "description": "Ocurrió un error que no pudo ser controlado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Ocurrió un error que no pudo ser controlado",
+                            "estado":"System Error"
+                        }
+                    } 
+                }       
+            },                       
+    } 
+)
+def search_unidades_pacto(finding : str = Query (min_length=os.getenv("MIN_STR_SEARCH_USER"), max_length=os.getenv("MAX_STR_SEARCH_USER")), page : int = 1, records : int = 20)->dict:
+    db = Session()
+    # almacenamos el listado de usarios en un resultset
+    result = UnidadesPactoController(db).search_unidades_pacto(finding,page,records)
+    # debemos convertir los objetos tipo BD a Json
+    if (result):
+        if (result["result"]=="1"):
+            data=result['data']
+            return JSONResponse(status_code=200,content=jsonable_encoder(data))    
+        elif (result["result"]=="-1"):
+            return JSONResponse(status_code=404,content={"message":"La búsqueda no arrojó resultados"})    
+        else:
+            return JSONResponse(status_code=520,content={"message":"System Error","error":result})          
+    else:
+        return JSONResponse(status_code=520,content={"message":"System Error","error":result})
+
+
+# ruta para actualizar una institución Bancaria por Id
+@unidades_pacto_router.put ('/bancos/{id}/update', 
+tags=["UnidadesPacto"],
+#dependencies=[Depends(JWTBearer())],
+responses=
+    { 
+        403: {
+            "description": "Forbiden",
+            "content": { 
+                "application/json":{ 
+                    "example":
+                        {
+                            "message":"Not authenticated"
+                        }
+                    } 
+                }       
+            },  
+        500: {
+            "description": "Su session ha expirado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Su session ha expirado",
+                            "estado":"Signature has expired"
+                        }
+                    } 
+                }       
+            },                        
+        520: {
+            "description": "Ocurrió un error que no pudo ser controlado",
+            "content": { 
+                "application/json":
+                    { "example":
+                        {
+                            "message":"Ocurrió un error que no pudo ser controlado",
+                            "estado":"System Error"
+                        }
+                    } 
+                }       
+            },                       
+    }
+)
+def update_unidades_pacto(banco:bancosSchema, user_updater: int = Query(ge=1, le=os.getenv('MAX_ID_USERS')), id : int = Path(ge=1,le=1000))->dict:
+    db = Session()
+    # buscamos el registro
+    result = UnidadesPactoController(db).update_unidades_pacto( banco, user_updater, id) 
+    if (result['result']=="1"):
+        data=result['data']
+        return JSONResponse(status_code=200,content={"message":"Banco actualizado","Banco":jsonable_encoder(data)})    
+    elif (result['result']=="-1"):
+        return JSONResponse(status_code=404,content={"message":"Banco no encontrado"}) 
+    else:
+        return JSONResponse(status_code=520,content={"message":"Ocurrió un error que no pudo ser controlado"})        
